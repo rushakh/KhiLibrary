6c035ba38dda9d2896e8212369713af9f464e75a
roham shirchi <rohamshirchi@hotmail.com>
2024-10-29 20:20:47 +0100

    Stable

--- KhiLibrary/AudioReader.cs
+++ KhiLibrary/AudioReader.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace KhiLibrary
 {
+    // For now This is Not used, But I plan to use this as way of lowering the responsibilities of MusicPlayer class and seperating the functionalities even more.
     internal class AudioReader : IWaveProvider
     {
         public WaveFormat WaveFormat => throw new NotImplementedException();
